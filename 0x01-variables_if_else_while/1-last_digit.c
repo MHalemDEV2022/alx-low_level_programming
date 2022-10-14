ce2ef833dde58177dd28aa5d104887905f7ea93a
MHalemDEV2022 <MHalem_DEV@outlook.com>
2022-10-14 11:41:15 -0700

    Task 1 : fixed the word greater not greter

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 
 	if (i > 5)
 	{
-		printf("Last digit of %d is %d and is greter than 5\n", n, i);
+		printf("Last digit of %d is %d and is greater than 5\n", n, i);
 	}
 	else if (i == 0)
 	{
