5c6dd48faed93f32ad2cf00ed3556949d620d6fe
MHalemDEV2022 <MHalem_DEV@outlook.com>
2022-10-20 14:17:05 -0700

    fixed N3

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,15 +5,17 @@
  *  from 0 to 14, followed by a new line.
  */
 void more_numbers(void)
-{
+i{
 	int i, n;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (n = 0; n < 14; n++)
-		{
+		{ii
 			if (n >= 10)
+			{
 				_putchar((n / 10) + '0');
+			}
 
 			_putchar((n % 10) + '0');
 		}
