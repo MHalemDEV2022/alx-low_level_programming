0828ed185ea4f0107ccc3bf27cbb6c5185dacf0c
MHalemDEV2022 <MHalem_DEV@outlook.com>
2022-10-20 14:09:32 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,10 +12,11 @@ void more_numbers(void)
 	{
 		for (n = 0; n < 14; n++)
 		{
-			_putchar(n + '0');
+			if (n >= 10)
+				_putchar((n / 10) + '0');
 		}
 
-		_puchar('\n');
+		_puchar((n % 10) + '\n');
 	}
 
 	_putchar('\n');
