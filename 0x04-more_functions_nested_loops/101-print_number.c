5c127544de82ae904a45c4665cd12f250c3aeb87
MHalemDEV2022 <MHalem_DEV@outlook.com>
2022-10-21 16:54:50 -0700

    fixed N10

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,35 +5,35 @@
  */
 void print_number(int n)
 {
-	int P10 = 1, ItoL = n;
+	int P10 = 1, num = n;
 	int Bool = 1;
 
-	if (ItoL < 0)
+	if (num < 0)
 	{
-		ItoL *= -1;
+		num *= -1;
 		_putchar('-');
 	}
 
-	if (ItoL >= 0 && ItoL <= 9)
-		_putchar(ItoL + '0');
+	if (num >= 0 && num <= 9)
+		_putchar(num + '0');
 	while (Bool)
 	{
-		if (ItoL / (P10 * 10) > 0)
+		if (num / (P10 * 10) > 0)
 			P10 *= 10;
 		else
 			Bool = 0;
 	}
 
-	while (ItoL >= 10)
+	while (num >= 10)
 	{
 		if (P10 == 1)
 		{
-			_putchar(ItoL % 10 + '0');
+			_putchar(num + '0');
 			ItoL = -1;
 		}
 		else
 		{
-			_putchar((ItoL / P10 % 10) + '0');
+			_putchar((num / P10 % 10) + '0');
 			P10 /= 10;
 		}
 	}
